062e57620a4dc3b44aac17e15f2d2827b37f2bd0
yang-wei <yangweilim21@gmail.com>
2016-09-04 00:10:53 +0900

    Rollbar

--- processUserQueue.js
+++ processUserQueue.js
@@ -5,6 +5,14 @@ const getUserGists = require('./getUserGists');
 const crawlGist = require('./crawlGist');
 const R = require('ramda');
 
+const rollbarToken = require('./credentials/rollbar.json').token;
+const rollbar = require('rollbar');
+
+if (process.env.NODE_ENV === 'production') {
+  rollbar.init(rollbarToken);
+  rollbar.reportMessage('Starting rollbar monitoting');
+}
+
 userQueue.process(function(job, done){
   const username = job.data;
   const sqlLog = (err, item) => { 
